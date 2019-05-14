5bea3eb0b748190101eb7c87a5ab5f9409ab43ad
TheRainstorm <33221728+TheRainstorm@users.noreply.github.com>
2019-05-14 22:20:36 +0800

    update recommend

--- recommend.py
+++ recommend.py
@@ -57,79 +57,6 @@ def recommend_hair(user_gender,user_face_type,\
     else:
         return [HAIR_WOMAN[index] for index in sorted_index]
 
-# def load_json(style_dir_path):
-# #得到数据库中的图片的脸型并存储
-#     data_path=os.path.join(style_dir_path,'data.json')
-#     if os.path.exists(data_path):
-#         with open(data_path) as file:
-#             Dict=json.load(file)
-#             return Dict
-#     else:
-#         Dict={}
-#         img_name_list=os.listdir(style_dir_path)
-#         counter=0
-#         print('totally: '+str(len(img_name_list)))
-#         for img_name in img_name_list:
-#             try:
-#                 print(counter)
-#                 counter+=1
-#                 img_full_path=os.path.join(style_dir_path,img_name)
-#                 #Dict[img_full_path]={}
-
-#                 img=io.imread(img_full_path)
-#                 shape=get_face_type(img)
-
-#                 Dict[img_full_path]=shape
-#             except:
-#                 pass
-#         with open(data_path,'w') as file:
-#             json.dump(Dict,file)
-#             return Dict
-
-# def face_shape_sort(user_face_type,user_points_array,style_dir_path):
-#     # user_img=io.imread(user_img_path)
-#     # user_face_type=get_face_type(user_img)
-#     # user_points_array=get_rotated_points_array(user_img)
-
-#     Dict=load_json(style_dir_path)
-
-#     L=[]
-#     img_name_list=os.listdir(style_dir_path)
-
-#     for img_name in img_name_list:
-#         try:
-#             img_full_path=os.path.join(style_dir_path,img_name)
-
-#             shape=Dict[img_full_path]
-#             if(shape==user_face_type):
-#                 L.append(img_full_path)
-#         except:
-#             pass
-#     D=[]
-#     for img_full_path in L:
-#         img=io.imread(img_full_path)
-#         points_array=get_rotated_points_array(img)
-
-#         distance= np.mean(np.square(points_array-user_points_array))
-#         D.append([img_full_path,distance])
-
-#     D.sort(key=lambda x:x[1])
-#     return [e[0] for e in D]
-
-# def recommend(user_gender, user_image_array, user_prefer_vector=[0.5, 0.5, 0.5, 0.5, 0.5]):
-#     user_face_type=get_face_type(user_image_array)
-#     user_points_array= get_rotated_points_array(user_image_array)
-
-#     style = recommend_hair(user_gender,user_face_type,user_prefer_vector)
-
-#     if user_gender==0:
-#         style_dir_path= 'database/man/' + style +'/'
-#     else:
-#         style_dir_path= 'database/woman/' + style +'/'
-
-#     L = face_shape_sort(user_face_type, user_points_array, style_dir_path)
-#     return L,user_face_type, style
-
 def load_json(style_dir_path):
 #得到数据库中的图片的脸型并存储
     data_path=os.path.join(style_dir_path,'data.json')
@@ -215,15 +142,21 @@ def recommend(user_gender, user_image_array, user_prefer_vector=[0.5, 0.5, 0.5, 
     return style_list,user_face_type
 
 if __name__=='__main__':
-    # os.makedirs('output/man/')
-    # os.makedirs('output/woman/')
-    # for style in HAIR_MAN:
-    #     if(not os.path.exists('output/man/'+style+'/')):
-    #         os.makedirs('output/man/'+style+'/')
-    # for style in HAIR_WOMAN:
-    #     if(not os.path.exists('output/woman/'+style+'/')):
-    #         os.makedirs('output/woman/'+style+'/')
+    #make dir
+    try:
+        os.makedirs('output/man/')
+        os.makedirs('output/woman/')
+    except:
+        pass
+    for style in HAIR_MAN:
+        if(not os.path.exists('output/man/'+style+'/')):
+            os.makedirs('output/man/'+style+'/')
+    for style in HAIR_WOMAN:
+        if(not os.path.exists('output/woman/'+style+'/')):
+            os.makedirs('output/woman/'+style+'/')
+
     #debug for recommend
+    #girl
     user_img_path='test_images/100.jpg'
     user_img = io.imread(user_img_path)
 
@@ -232,7 +165,7 @@ if __name__=='__main__':
     print("User face type: "+user_face_type)
     print("recommend style"+str(style_list))
 
-
+    #boy
     user_img_path='test_images/52.jpg'
     user_img = io.imread(user_img_path)
 
